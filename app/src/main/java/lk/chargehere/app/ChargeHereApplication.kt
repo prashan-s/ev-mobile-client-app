69f39ae6aae9093d6bcd1d5732c0cff9990c6676
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-09-25 06:33:07 +0530

    Add application and main activity

---
+++ app/src/main/java/lk/chargehere/app/ChargeHereApplication.kt
@@ -0,0 +1,7 @@
+package lk.chargehere.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ChargeHereApplication : Application()
---
+++ app/src/main/java/lk/chargehere/app/MainActivity.kt
@@ -0,0 +1,52 @@
+package lk.chargehere.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import lk.chargehere.app.auth.GoogleSignInManager
+import lk.chargehere.app.navigation.ChargeHereNavigation
+import lk.chargehere.app.navigation.NavigationGraph
+import lk.chargehere.app.ui.theme.ChargeHereTheme
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var googleSignInManager: GoogleSignInManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize Google Sign-In
+        googleSignInManager.initialize(this)
+
+        enableEdgeToEdge()
+        setContent {
+            ChargeHereTheme {
+                ChargeHereApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun ChargeHereApp() {
+    val navController = rememberNavController()
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        ChargeHereNavigation(
+            navController = navController,
+            startDestination = NavigationGraph.Auth.route,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
