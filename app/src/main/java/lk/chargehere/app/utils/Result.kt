00970d3054100da978fd5184e3bad4296ced2178
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-09-27 18:39:54 +0530

    Add utility functions

---
+++ app/src/main/java/lk/chargehere/app/utils/Result.kt
@@ -0,0 +1,22 @@
+package lk.chargehere.app.utils
+
+sealed class Result<out T> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val message: String) : Result<Nothing>()
+    data object Loading : Result<Nothing>()
+}
+
+inline fun <T> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
+    if (this is Result.Success) action(data)
+    return this
+}
+
+inline fun <T> Result<T>.onError(action: (message: String) -> Unit): Result<T> {
+    if (this is Result.Error) action(message)
+    return this
+}
+
+inline fun <T> Result<T>.onLoading(action: () -> Unit): Result<T> {
+    if (this is Result.Loading) action()
+    return this
+}
